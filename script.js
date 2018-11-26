5f41f89076bb1868b5f811b1cd0ad0f0c1301947
Msiemax <38659429+Msiemax@users.noreply.github.com>
2018-11-26 10:49:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+
+//Home arrow
 $("#arrow").click(function () { 
 	$('html,body').animate({ 
 		scrollTop: $("main").offset().top 
 	},
 		1200);
 });
+
+
+//About me arrow
+$(document).ready(function(){
+	$("#arrow2").click(function(){
+		$('html ,body').animate({scrollTop : 0},1200);
+	});
+});
+
+//Gallery arrow
+$("#arrowww").click(function () { 
+	$('html,body').animate({ 
+		scrollTop: $("section").offset().top 
+	},
+		1200);
+});
+
+
+//navigation
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+
+	var currentScrollpos = window.pageYOffset;
+	if(prevScrollpos > currentScrollpos) {
+		document.getElementById("navv").style.top = "0";
+
+	}	else {
+		document.getElementById("navv").style.top = "-100px";
+	}
+
+	prevScrollpos = currentScrollpos;
+}
